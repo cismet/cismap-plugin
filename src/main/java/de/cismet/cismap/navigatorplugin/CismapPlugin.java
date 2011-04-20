c5b0c08cdb01552caf39495f8c69e56af4b1fbc9
Jens Weintraut <jens.weintraut@cismet.de>
2011-04-20 13:23:00 +0000

    [issue:2052] - added: getter for CismapPlugin's ConfigurationManager

--- src/main/java/de/cismet/cismap/navigatorplugin/CismapPlugin.java
+++ src/main/java/de/cismet/cismap/navigatorplugin/CismapPlugin.java
@@ -1758,6 +1758,15 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
 
     /**
      * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public ConfigurationManager getConfigurationManager() {
+        return configurationManager;
+    }
+
+    /**
+     * DOCUMENT ME!
      */
     private void setupDefaultLayout() {
         if (wfsViews.length != 0) {
@@ -3322,27 +3331,27 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniSearchCidsFeatureActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniSearchCidsFeatureActionPerformed
+    private void mniSearchCidsFeatureActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniSearchCidsFeatureActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_mniSearchCidsFeatureActionPerformed
+    } //GEN-LAST:event_mniSearchCidsFeatureActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniSearchPolygonActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniSearchPolygonActionPerformed
+    private void mniSearchPolygonActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniSearchPolygonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_mniSearchPolygonActionPerformed
+    } //GEN-LAST:event_mniSearchPolygonActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniSearchCidsFeature1ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniSearchCidsFeature1ActionPerformed
+    private void mniSearchCidsFeature1ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniSearchCidsFeature1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_mniSearchCidsFeature1ActionPerformed
+    } //GEN-LAST:event_mniSearchCidsFeature1ActionPerformed
 
     /**
      * DOCUMENT ME!
