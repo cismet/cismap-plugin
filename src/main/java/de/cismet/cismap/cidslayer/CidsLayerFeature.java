f809d88c6dbee771ec9d76b87e91495d616ec44a
Matthias Roncoroni <matthias.roncoroni@cismet.de>
2013-08-28 12:02:27 +0200

    #23 implemented the most funktions from SLD

--- src/main/java/de/cismet/cismap/cidslayer/CidsLayerFeature.java
+++ src/main/java/de/cismet/cismap/cidslayer/CidsLayerFeature.java
@@ -23,6 +23,37 @@ import edu.umd.cs.piccolo.PCamera;
 import edu.umd.cs.piccolo.nodes.PImage;
 
 import org.apache.log4j.Logger;
+import org.apache.xerces.xs.XSElementDeclaration;
+
+import org.deegree.commons.tom.TypedObjectNode;
+import org.deegree.commons.tom.gml.GMLObjectCategory;
+import org.deegree.commons.tom.gml.GMLObjectType;
+import org.deegree.commons.tom.gml.property.Property;
+import org.deegree.commons.tom.gml.property.PropertyType;
+import org.deegree.commons.tom.primitive.BaseType;
+import org.deegree.commons.tom.primitive.PrimitiveType;
+import org.deegree.commons.tom.primitive.PrimitiveValue;
+import org.deegree.commons.uom.Measure;
+import org.deegree.commons.uom.Unit;
+import org.deegree.cs.coordinatesystems.ICRS;
+import org.deegree.feature.Feature;
+import org.deegree.feature.property.ExtraProps;
+import org.deegree.feature.types.AppSchema;
+import org.deegree.feature.types.FeatureType;
+import org.deegree.feature.types.property.GeometryPropertyType;
+import org.deegree.filter.FilterEvaluationException;
+import org.deegree.filter.expression.ValueReference;
+import org.deegree.geometry.Envelope;
+import org.deegree.geometry.precision.PrecisionModel;
+import org.deegree.geometry.primitive.Point;
+import org.deegree.style.se.unevaluated.Style;
+import org.deegree.style.styling.PointStyling;
+import org.deegree.style.styling.PolygonStyling;
+import org.deegree.style.styling.Styling;
+import org.deegree.style.styling.TextStyling;
+
+import org.jfree.util.Log;
+
 
 import java.util.List;
 import java.util.Map;
@@ -43,11 +74,14 @@ import de.cismet.cismap.commons.gui.piccolo.PSticky;
  * @author   mroncoroni
  * @version  $Revision$, $Date$
  */
-public class CidsLayerFeature extends DefaultFeatureServiceFeature {
+public class CidsLayerFeature extends DefaultFeatureServiceFeature implements SLDStyledFeature {
 
     //~ Static fields/initializers ---------------------------------------------
 
     private static transient Logger LOG = Logger.getLogger(CidsLayerFeature.class);
+    private static final String OBJECT_ID = "object_id";
+    private static final String CLASS_ID = "class_id";
+    private static final String GEOMETRIE = "geo_field";
 
     //~ Instance fields --------------------------------------------------------
 
