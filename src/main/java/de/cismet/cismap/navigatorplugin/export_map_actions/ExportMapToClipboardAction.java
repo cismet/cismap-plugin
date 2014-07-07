f06094b8bcf4de2f944b85923dbe4046e070e9c7
Gilles Baatz <gilles.baatz@cismet.de>
2014-07-07 16:32:21 +0200

    #37 ignore dpi and file format if the map is exported to the clipboard

--- src/main/java/de/cismet/cismap/navigatorplugin/export_map_actions/ExportMapToClipboardAction.java
+++ src/main/java/de/cismet/cismap/navigatorplugin/export_map_actions/ExportMapToClipboardAction.java
@@ -12,20 +12,9 @@ import org.openide.util.NbBundle;
 import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
-import java.awt.image.BufferedImage;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import javax.imageio.ImageIO;
+import de.cismet.cismap.navigatorplugin.ImageSelection;
 
 import de.cismet.tools.gui.StaticSwingTools;
 
@@ -82,26 +71,9 @@ public class ExportMapToClipboardAction extends AbstractExportMapAction {
                                 }
                             });
 
-                        try {
-                            final Image image = ((Future<Image>)getFutureImageFromMapViaHeadlessMapProvider()[0]).get();
-                            final ByteArrayOutputStream byteoutarray = new ByteArrayOutputStream();
-                            ImageIO.write(
-                                (BufferedImage)image,
-                                exportMapDataProvider.getFileType().getImageFileExtension(),
-                                byteoutarray);
-
-                            final ImageAsByteArraySelection trans = new ImageAsByteArraySelection(
-                                    byteoutarray.toByteArray());
-
-                            final Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                            clipboard.setContents(trans, null);
-                        } catch (InterruptedException ex) {
-                            LOG.error(ex);
-                        } catch (ExecutionException ex) {
-                            LOG.error(ex);
-                        } catch (IOException ex) {
-                            LOG.error(ex);
-                        }
+                        final Image image = getMapC().getImage();
+                        final ImageSelection imgSel = new ImageSelection(image);
+                        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(imgSel, null);
 
                         EventQueue.invokeLater(new Runnable() {
 
@@ -113,50 +85,5 @@ public class ExportMapToClipboardAction extends AbstractExportMapAction {
                     }
                 });
         t.start();
-    }
-
-    //~ Inner Classes ----------------------------------------------------------
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @version  $Revision$, $Date$
-     */
-    public static class ImageAsByteArraySelection implements Transferable {
-
-        //~ Instance fields ----------------------------------------------------
-
-        private final byte[] data;
-
-        //~ Constructors -------------------------------------------------------
-
-        /**
-         * Creates a new ImageAsByteArraySelection object.
-         *
-         * @param  data  DOCUMENT ME!
-         */
-        public ImageAsByteArraySelection(final byte[] data) {
-            this.data = data;
-        }
-
-        //~ Methods ------------------------------------------------------------
-
-        @Override
-        public DataFlavor[] getTransferDataFlavors() {
-            return new DataFlavor[] { DataFlavor.imageFlavor };
-        }
-
-        @Override
-        public boolean isDataFlavorSupported(final DataFlavor flavor) {
-            return DataFlavor.imageFlavor.equals(flavor);
-        }
-
-        @Override
-        public Object getTransferData(final DataFlavor flavor) throws UnsupportedFlavorException, IOException {
-            if (!DataFlavor.imageFlavor.equals(flavor)) {
-                throw new UnsupportedFlavorException(flavor);
-            }
-            return Toolkit.getDefaultToolkit().createImage(data);
-        }
     }
 }
