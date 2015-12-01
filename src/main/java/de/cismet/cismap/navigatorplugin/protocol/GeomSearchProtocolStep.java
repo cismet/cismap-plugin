2912fde74086de8b139d0dfe77d7186e95ae9d4a
JeanAtCismet <jean.ruiz@cismet.de>
2015-12-01 13:02:29 +0100

    issue #56 map visualization with configuration

--- src/main/java/de/cismet/cismap/navigatorplugin/protocol/GeomSearchProtocolStep.java
+++ src/main/java/de/cismet/cismap/navigatorplugin/protocol/GeomSearchProtocolStep.java
@@ -32,6 +32,7 @@ public interface GeomSearchProtocolStep extends ProtocolStep {
      * @return  DOCUMENT ME!
      */
     Geometry getGeometry();
+    
     @Override
     GeomSearchProtocolStepConfiguration getConfiguration();
 }
