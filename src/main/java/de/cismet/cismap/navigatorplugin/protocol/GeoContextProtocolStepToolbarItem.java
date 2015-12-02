3fc74eed3817f50e07db5dcacaa459fac94e8259
JeanAtCismet <jean.ruiz@cismet.de>
2015-12-02 15:12:15 +0100

    issue #56 lookupable toolbar item for GeoContext step

---
+++ src/main/java/de/cismet/cismap/navigatorplugin/protocol/GeoContextProtocolStepToolbarItem.java
@@ -0,0 +1,74 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cismap.navigatorplugin.protocol;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+
+import de.cismet.cismap.commons.XBoundingBox;
+import de.cismet.cismap.commons.interaction.CismapBroker;
+
+import de.cismet.commons.gui.protocol.AbstractProtocolStepToolbarItemAction;
+import de.cismet.commons.gui.protocol.ProtocolHandler;
+import de.cismet.commons.gui.protocol.ProtocolStepToolbarItem;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = ProtocolStepToolbarItem.class)
+public class GeoContextProtocolStepToolbarItem extends AbstractProtocolStepToolbarItemAction {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final String NAME = "";
+    private static final String TOOLTIP = "Geo-Kontext protokollieren";
+    private static final ImageIcon ICON = new ImageIcon(GeoContextProtocolStepToolbarItem.class.getResource(
+                "/Sirius/navigator/resource/img/map.png"));
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new CommentProtocolStepToolbarItem object.
+     */
+    public GeoContextProtocolStepToolbarItem() {
+        super(NAME, TOOLTIP, null, ICON);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getSorterString() {
+        return "ZZZ";
+    }
+
+    @Override
+    public boolean isVisible() {
+        return true;
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        final Geometry geometry =
+            ((XBoundingBox)CismapBroker.getInstance().getMappingComponent().getCurrentBoundingBoxFromCamera())
+                    .getGeometry();
+        ProtocolHandler.getInstance().recordStep(new GeoContextProtocolStepImpl(geometry), false);
+    }
+}
