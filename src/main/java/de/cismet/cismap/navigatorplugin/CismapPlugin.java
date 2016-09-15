66f591ddc1891b547465f17d870bc4959e1b1c95
JeanAtCismet <jean.ruiz@cismet.de>
2016-09-15 19:12:06 +0200

    issue #75 temporary constructor code removed

--- src/main/java/de/cismet/cismap/navigatorplugin/CismapPlugin.java
+++ src/main/java/de/cismet/cismap/navigatorplugin/CismapPlugin.java
@@ -1131,11 +1131,6 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
                 loadLayout(cismapDirectory + fs + pluginLayoutName, true);
             }
         }
-
-        // TODO not handle this here !
-        final SelectionListener sl = (SelectionListener)mapC.getInputEventListener().get(MappingComponent.SELECT);
-        sl.setFeaturesFromServicesSelectable(true);
-        sl.setSelectMultipleFeatures(true);
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -2850,7 +2845,7 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cmdNewLinearReferencingcreateGeometryAction(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cmdNewLinearReferencingcreateGeometryAction
+    private void cmdNewLinearReferencingcreateGeometryAction(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdNewLinearReferencingcreateGeometryAction
         EventQueue.invokeLater(new Runnable() {
 
                 @Override
@@ -2858,14 +2853,14 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
                     mapC.setInteractionMode(MappingComponent.LINEAR_REFERENCING);
                 }
             });
-    } //GEN-LAST:event_cmdNewLinearReferencingcreateGeometryAction
+    }//GEN-LAST:event_cmdNewLinearReferencingcreateGeometryAction
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniBufferSelectedGeomActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniBufferSelectedGeomActionPerformed
+    private void mniBufferSelectedGeomActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniBufferSelectedGeomActionPerformed
         final Collection c = mapC.getFeatureCollection().getSelectedFeatures();
         if ((c != null) && (c.size() > 0)) {
             final String s = (String)JOptionPane.showInputDialog(
@@ -2922,14 +2917,14 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
                     "CismapPlugin.mniBufferSelectedGeom.Dialog.title"), // NOI18N
                 JOptionPane.WARNING_MESSAGE);
         }
-    }                                                                   //GEN-LAST:event_mniBufferSelectedGeomActionPerformed
+    }//GEN-LAST:event_mniBufferSelectedGeomActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cmdNodeReflectGeometryActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cmdNodeReflectGeometryActionPerformed
+    private void cmdNodeReflectGeometryActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdNodeReflectGeometryActionPerformed
         EventQueue.invokeLater(new Runnable() {
 
                 @Override
@@ -2938,23 +2933,23 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
                     mapC.setInteractionMode(MappingComponent.SELECT);
                 }
             });
-    } //GEN-LAST:event_cmdNodeReflectGeometryActionPerformed
+    }//GEN-LAST:event_cmdNodeReflectGeometryActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniAngleMeasurementActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniAngleMeasurementActionPerformed
+    private void mniAngleMeasurementActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniAngleMeasurementActionPerformed
         StaticSwingTools.showDialog(AngleMeasurementDialog.getInstance());
-    }                                                                                       //GEN-LAST:event_mniAngleMeasurementActionPerformed
+    }//GEN-LAST:event_mniAngleMeasurementActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void mniLoadShapeActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniLoadShapeActionPerformed
+    private void mniLoadShapeActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniLoadShapeActionPerformed
         JFileChooser fc;
 
         try {
@@ -2989,14 +2984,14 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
             final ActiveLayerModel model = (ActiveLayerModel)mapC.getMappingModel();
             LayerDropUtils.handleFiles(Collections.nCopies(1, file), model, 0, this);
         }
-    } //GEN-LAST:event_mniLoadShapeActionPerformed
+    }//GEN-LAST:event_mniLoadShapeActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cmdSelectSingleActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cmdSelectSingleActionPerformed
+    private void cmdSelectSingleActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdSelectSingleActionPerformed
         EventQueue.invokeLater(new Runnable() {
 
                 @Override
@@ -3006,7 +3001,7 @@ public class CismapPlugin extends javax.swing.JFrame implements PluginSupport,
                         SelectionListener.RECTANGLE);
                 }
             });
-    } //GEN-LAST:event_cmdSelectSingleActionPerformed
+    }//GEN-LAST:event_cmdSelectSingleActionPerformed
 
     /**
      * DOCUMENT ME!
