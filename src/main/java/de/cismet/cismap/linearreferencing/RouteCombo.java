334584d988409220bd203bc4f8c7e517e8a34653
therter <thorsten.herter@cismet.de>
2017-09-15 09:26:33 +0200

    #94 use the CalculationCache to determine the routes

--- src/main/java/de/cismet/cismap/linearreferencing/RouteCombo.java
+++ src/main/java/de/cismet/cismap/linearreferencing/RouteCombo.java
@@ -12,21 +12,28 @@
  */
 package de.cismet.cismap.linearreferencing;
 
+import Sirius.navigator.connection.SessionManager;
+
 import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.middleware.types.MetaObject;
 
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import org.jdesktop.swingx.autocomplete.ObjectToStringConverter;
 
 import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.event.FocusListener;
 
 import java.lang.ref.SoftReference;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListCellRenderer;
@@ -48,6 +55,9 @@ import de.cismet.cismap.commons.interaction.CismapBroker;
 import de.cismet.cismap.commons.retrieval.RetrievalEvent;
 import de.cismet.cismap.commons.retrieval.RetrievalListener;
 
+import de.cismet.tools.CalculationCache;
+import de.cismet.tools.Calculator;
+
 import static de.cismet.cismap.linearreferencing.LinearReferencingSingletonInstances.LOG;
 
 /**
@@ -62,15 +72,19 @@ public class RouteCombo extends javax.swing.JPanel {
 
     private static Map<XBoundingBox, SoftReference<List<Feature>>> cache =
         new HashMap<XBoundingBox, SoftReference<List<Feature>>>();
+    private static Date lastRefresh = new Date();
+    public static final CalculationCache<RouteParameter, List<Feature>> routeCache =
+        new CalculationCache<RouteParameter, List<Feature>>(new RouteCalculator());
 
     //~ Instance fields --------------------------------------------------------
 
     private LinearReferencingHelper linearReferencingHelper = FeatureRegistry.getInstance()
                 .getLinearReferencingSolver();
     private final String routeMetaClassName;
-    private boolean routesComboInitialised = false;
+    private Date routesComboInitialised = null;
     private String routeNamePropertyName;
     private String routeQuery;
+    private List<FocusListener> focusListener = new ArrayList<FocusListener>();
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox cbPossibleRoute;
@@ -152,6 +166,20 @@ public class RouteCombo extends javax.swing.JPanel {
         return routeQuery;
     }
 
+    @Override
+    public synchronized void addFocusListener(final FocusListener l) {
+        super.addFocusListener(l);
+
+        focusListener.add(l);
+    }
+
+    @Override
+    public synchronized void removeFocusListener(final FocusListener l) {
+        super.removeFocusListener(l);
+
+        focusListener.remove(l);
+    }
+
     /**
      * DOCUMENT ME!
      *
@@ -167,7 +195,11 @@ public class RouteCombo extends javax.swing.JPanel {
      * @param  value  DOCUMENT ME!
      */
     public void fillRoutesCombo(final Object value) {
-        routesComboInitialised = true;
+        if ((routesComboInitialised != null) && routesComboInitialised.after(lastRefresh)) {
+            return;
+        }
+
+        routesComboInitialised = new Date();
         cbPossibleRoute.setModel(new DefaultComboBoxModel(new Object[] { "Lade" }));
         final MetaClass routeMc = ClassCacheMultiple.getMetaClass(
                 linearReferencingHelper.getDomainOfRouteTable(routeMetaClassName)[0],
@@ -185,51 +217,75 @@ public class RouteCombo extends javax.swing.JPanel {
         if (featureList != null) {
             fillComboBox(featureList, value);
         } else {
-            final CidsLayer layer = new CidsLayer(routeMc);
-            try {
-//                layer.setBoundingBox(currentBoundingBox);
-                if (routeQuery != null) {
-                    layer.setQuery(routeQuery);
-                }
-                layer.setBoundingBox(null);
-                layer.addRetrievalListener(new RetrievalListener() {
+//            final CidsLayer layer = new CidsLayer(routeMc);
+//            try {
+////                layer.setBoundingBox(currentBoundingBox);
+//                if (routeQuery != null) {
+//                    layer.setQuery(routeQuery);
+//                }
+//                layer.setBoundingBox(null);
+//                layer.addRetrievalListener(new RetrievalListener() {
+//
+//                        @Override
+//                        public void retrievalStarted(final RetrievalEvent e) {
+//                        }
+//
+//                        @Override
+//                        public void retrievalProgress(final RetrievalEvent e) {
+//                        }
+//
+//                        @Override
+//                        public void retrievalComplete(final RetrievalEvent e) {
+//                            if (!e.isInitialisationEvent()) {
+//                                complete((List)e.getRetrievedObject());
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void retrievalAborted(final RetrievalEvent e) {
+//                            complete((List)e.getRetrievedObject());
+//                        }
+//
+//                        @Override
+//                        public void retrievalError(final RetrievalEvent e) {
+//                            complete((List)e.getRetrievedObject());
+//                        }
+//
+//                        private void complete(final List features) {
+//                            cache.clear();
+//                            cache.put(currentBoundingBox, new SoftReference<List<Feature>>(features));
+//                            fillComboBox(features, value);
+//                        }
+//                    });
+//
+//                layer.retrieve(true);
+//                LOG.error("Lade Routen");
+//            } catch (Exception e) {
+//                LOG.error("Error while retrieving features", e);
+//            }
+            final Thread t = new Thread("Load Routes") {
 
-                        @Override
-                        public void retrievalStarted(final RetrievalEvent e) {
-                        }
-
-                        @Override
-                        public void retrievalProgress(final RetrievalEvent e) {
-                        }
-
-                        @Override
-                        public void retrievalComplete(final RetrievalEvent e) {
-                            if (!e.isInitialisationEvent()) {
-                                complete((List)e.getRetrievedObject());
-                            }
-                        }
-
-                        @Override
-                        public void retrievalAborted(final RetrievalEvent e) {
-                            complete((List)e.getRetrievedObject());
-                        }
-
-                        @Override
-                        public void retrievalError(final RetrievalEvent e) {
-                            complete((List)e.getRetrievedObject());
-                        }
-
-                        private void complete(final List features) {
+                    @Override
+                    public void run() {
+                        try {
+                            final RouteParameter parameter = new RouteParameter(routeMc, routeQuery);
+                            final List<Feature> features = routeCache.calcValue(parameter);
                             cache.clear();
                             cache.put(currentBoundingBox, new SoftReference<List<Feature>>(features));
-                            fillComboBox(features, value);
-                        }
-                    });
 
-                layer.retrieve(true);
-            } catch (Exception e) {
-                LOG.error("Error while retrieving features", e);
-            }
+                            EventQueue.invokeLater(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        fillComboBox(features, value);
+                                    }
+                                });
+                        } catch (Exception e) {
+                            LOG.error("Error while retrieving features", e);
+                        }
+                    }
+                };
+            t.start();
         }
     }
 
@@ -238,6 +294,7 @@ public class RouteCombo extends javax.swing.JPanel {
      */
     public static void clearRouteCache() {
         cache.clear();
+        lastRefresh = new Date();
     }
 
     /**
@@ -407,6 +464,13 @@ public class RouteCombo extends javax.swing.JPanel {
         cbPossibleRoute.setModel(new javax.swing.DefaultComboBoxModel(
                 new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbPossibleRoute.setPreferredSize(new java.awt.Dimension(300, 20));
+        cbPossibleRoute.addFocusListener(new java.awt.event.FocusAdapter() {
+
+                @Override
+                public void focusLost(final java.awt.event.FocusEvent evt) {
+                    cbPossibleRouteFocusLost(evt);
+                }
+            });
         cbPossibleRoute.addActionListener(new java.awt.event.ActionListener() {
 
                 @Override
@@ -429,6 +493,133 @@ public class RouteCombo extends javax.swing.JPanel {
      * @param  evt  DOCUMENT ME!
      */
     private void cbPossibleRouteActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cbPossibleRouteActionPerformed
-        // TODO add your handling code here:
-    } //GEN-LAST:event_cbPossibleRouteActionPerformed
+        for (final FocusListener l : focusListener) {
+            l.focusLost(null);
+        }
+    }                                                                                   //GEN-LAST:event_cbPossibleRouteActionPerformed
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  evt  DOCUMENT ME!
+     */
+    private void cbPossibleRouteFocusLost(final java.awt.event.FocusEvent evt) { //GEN-FIRST:event_cbPossibleRouteFocusLost
+        for (final FocusListener l : focusListener) {
+            l.focusLost(evt);
+        }
+    }                                                                            //GEN-LAST:event_cbPossibleRouteFocusLost
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private static class RouteCalculator implements Calculator<RouteParameter, List<Feature>> {
+
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public List<Feature> calculate(final RouteParameter parameter) throws Exception {
+            final CidsLayer layer = new CidsLayer(parameter.getRouteMc());
+            final String routeQuery = parameter.getQuery();
+            if (routeQuery != null) {
+                layer.setQuery(routeQuery);
+            }
+
+            layer.setBoundingBox(null);
+            layer.initAndWait();
+
+            return layer.getFeatureFactory().createFeatures(routeQuery, null, null, 0, 0, null);
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private static class RouteParameter {
+
+        //~ Instance fields ----------------------------------------------------
+
+        private MetaClass routeMc;
+        private String query;
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new RouteParameter object.
+         *
+         * @param  routeMc  DOCUMENT ME!
+         * @param  query    DOCUMENT ME!
+         */
+        public RouteParameter(final MetaClass routeMc, final String query) {
+            this.routeMc = routeMc;
+            this.query = query;
+        }
+
+        //~ Methods ------------------------------------------------------------
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @return  the routeMc
+         */
+        public MetaClass getRouteMc() {
+            return routeMc;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @param  routeMc  the routeMc to set
+         */
+        public void setRouteMc(final MetaClass routeMc) {
+            this.routeMc = routeMc;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @return  the query
+         */
+        public String getQuery() {
+            return query;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @param  query  the query to set
+         */
+        public void setQuery(final String query) {
+            this.query = query;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj instanceof RouteParameter) {
+                final RouteParameter other = (RouteParameter)obj;
+                final boolean sameQuery = (((query == null) && (other.getQuery() == null))
+                                || ((query != null) && (other.getQuery() != null) && query.equals(other.getQuery())));
+                final boolean sameMc = (((routeMc == null) && (other.getRouteMc() == null))
+                                || ((routeMc != null) && (other.getRouteMc() != null)
+                                    && routeMc.equals(other.getRouteMc())));
+
+                return sameQuery && sameMc;
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 3;
+            hash = (97 * hash) + Objects.hashCode(this.routeMc);
+            hash = (97 * hash) + Objects.hashCode(this.query);
+            return hash;
+        }
+    }
 }
